97f35d5f1c97f70d8fea053e2f0b819989c04ad5
Zane <zeisen33@gmail.com>
2022-12-23 13:55:14 -0500

    User routing done

--- reddit/app/controllers/users_controller.rb
+++ reddit/app/controllers/users_controller.rb
@@ -7,17 +7,17 @@ class UsersController < ApplicationController
 
     def create
         # render :create
-        @user = User.new(user_params)
-        if @user.save
-            login!(@user)
-            redirect_to subs_url
+        user = User.new(user_params)
+        if user.save
+            login!(user)
+            redirect_to user_url(user)
         else
             redirect_to new_user_url
         end
     end
 
     def show
-        @user = User.find_by(params[:id])
+        @user = User.find_by(id: params[:id])
         if @user
             render :show
         else
